9e02f81a338ece9d000a898064e27677f4f6cee0
Valentin Gassenmeier <vgassenme@student.ethz.ch>
2022-04-28 18:11:49 +0200

    fix

--- Temp_baseline.py
+++ Temp_baseline.py
@@ -1,9 +1,9 @@
-from keithley2600.keithley_driver import Keithley2600
+from keithley2600.keithley_driver import Keithley2600 as Kdr
 import data
 
 def svbd_baseline():
     #voltages, currents = (range(-2000,2010,10),range(-200,201))
-    voltages,currents = voltage_sweep_single_smu(smua, range(-200, 201), 1, 0.5, False,)
+    voltages,currents = Kdr.voltage_sweep_single_smu(smua, range(-200, 201), 1, 0.5, False,)
     with open("SteadyV_BD_Baseline.txt","w") as txt_file:
         for i in currents:
             txt_file.write(str(i)+"\n")
