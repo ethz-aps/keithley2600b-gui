9d331947c19bebce842cd5d75197787a69cde31d
Valentin Gassenmeier <vgassenme@student.ethz.ch>
2022-05-06 11:53:43 +0200

    Small Fix

--- steadyv_bd.py
+++ steadyv_bd.py
@@ -80,7 +80,7 @@ class SteadyV_BD (QWidget):
         ticks= np.logspace(0, 9)
         #self.disableSIPrefix
         self.graphWidget.setLogMode(y=True)
-        self.graphWidget.setRange(yRange=(1*10**(-12), 9*10**(0)), disableAutoRange=True)
+        self.graphWidget.setRange(disableAutoRange=True)
         self.graphWidget.enableAutoRange(axis='x')
         yax = self.graphWidget.getAxis('left')
         yax.setTicks([[(v,str(v)) for v in ticks]])
